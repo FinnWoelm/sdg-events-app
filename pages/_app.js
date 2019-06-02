25a9866252e0b6ff4c64ae27812a5bc06e0e62a6
Finn Woelm <public@finnwoelm.com>
2019-06-02 15:54:06 +0200

    Alias deployments to SDG events

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ class MyApp extends App {
     return (
       <Container>
         <Head>
-          <title>My page</title>
+          <title>SDG Events: Veranstaltungen zu Agenda 2030 in Deutschland</title>
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
