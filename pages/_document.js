05d658254c2cc7cc8efe925bb712a3d4118b254c
Finn Woelm <public@finnwoelm.com>
2019-06-02 15:25:22 +0200

    Set up Material UI
    Set up Material UI following the example repo:
    https://github.com/MarchWorks/nextjs-with-material-ui-and-styled-components

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,63 @@
-import Document from 'next/document'
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
 import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets } from '@material-ui/styles';
+import theme from '../config/theme';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage;
 
     try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
+        ctx.renderPage = () => originalRenderPage({
+            enhanceApp: App => props => ({...sheet.collectStyles(<App {...props} />), ...sheets.collect(<App {...props} />)})
+          })
+        const initialProps = await Document.getInitialProps(ctx)
+        return {
+          ...initialProps,
+          styles: (
+            <React.Fragment>
+              {sheets.getStyleElement()}
+              {sheet.getStyleElement()}
+              {flush() || null}
+            </React.Fragment>
+          )
+        }
+      } finally {
+        sheet.seal()
       }
-    } finally {
-      sheet.seal()
-    }
+  }
+
+  render() {
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta charSet="utf-8" />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={theme.palette.primary.main}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
+
+export default MyDocument;
