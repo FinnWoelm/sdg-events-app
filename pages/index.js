8b7cee573aee3337a07d8fb1d276a486625d8572
Finn Woelm <public@finnwoelm.com>
2019-06-04 13:03:25 +0200

    Home: Style hero banner
    Add more spacing & add SDG color wheel

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,41 @@ import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import styled from 'styled-components'
 
+import theme from 'config/theme'
 import Database from 'models/Database'
 import Event from 'models/Event'
 import EventGroup from 'components/EventGroup'
 
+const Hero = styled(Box)`
+  margin-top: 10vh;
+`
+
+const SearchBar = styled(Box)`
+  // position: sticky;
+  // top: 0;
+  margin-bottom: 10vh;
+`
+
+const Image = styled.img`
+  max-height: 100px;
+  margin: 16px 0;
+
+  ${theme.breakpoints.up('md')} {
+    min-height: 150px;
+  }
+
+  ${theme.breakpoints.up('lg')} {
+    min-height: 200px;
+  }
+`
+
 const Index = ({ groupedEvents }) => (
   <Container>
-    <Box textAlign='center'>
-      <Typography variant='h2'>
+    <Hero textAlign='center' paddingTop={2} paddingBottom={2}>
+      <Image src='/static/sdg-logo.png' alt='SDG color wheel'/>
+      <Typography variant='h2' gutterBottom>
         Events zu SDGs & Agenda 2030
       </Typography>
       <Typography variant='h4' color='textSecondary' style={{fontWeight: '300'}}>
@@ -18,7 +44,12 @@ const Index = ({ groupedEvents }) => (
         <br/>
         &mdash; t&auml;glich aktualisiert
       </Typography>
-    </Box>
+    </Hero>
+    <SearchBar>
+      {
+        // Search
+      }
+    </SearchBar>
     {Object.keys(groupedEvents).map(group => (
       <EventGroup key={group} title={group} events={groupedEvents[group]} />
     ))}
