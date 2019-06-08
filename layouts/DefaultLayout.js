ea6cb5e1b6b4b1dabcfb038e2037c606b003052a
Finn Woelm <public@finnwoelm.com>
2019-06-08 16:30:33 +0200

    Make footer stick to bottom

--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -1,16 +1,29 @@
 import { Fragment } from 'react'
 import Box from '@material-ui/core/Box'
+import { createGlobalStyle } from "styled-components"
 
 import Navigation from 'components/Navigation'
 import Footer from 'components/Footer'
 
+const StickyFooterStyles = createGlobalStyle`
+  html, body, #__next {
+    height: 100%;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
 const DefaultLayout = ({ children }) => (
   <Fragment>
     <Navigation />
-    <Box paddingTop={10} paddingBottom={8}>
+    <Box paddingTop={10} paddingBottom={8} style={{flexGrow: 1}}>
       {children}
     </Box>
     <Footer />
+    <StickyFooterStyles />
   </Fragment>
 )
 
